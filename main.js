4ce25c267c1c17023884b636d882810b5687717d
Anoosha Naz <163389690+Anoosh-Naz@users.noreply.github.com>
2024-03-25 10:52:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+const randonNumber = 9;
+const answer = await inquirer.prompt([{
+        name: "UserguessNumber",
+        type: "number",
+        message: "please guess a number"
+    }]);
+if (answer.UserguessNumber === randonNumber) {
+    console.log("congratulation you guessed right number");
+}
+else {
+    console.log("you guessed wrong number");
+}
